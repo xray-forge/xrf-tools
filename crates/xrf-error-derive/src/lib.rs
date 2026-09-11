35da70c63d9c591c94de0e7e148a25e808cd8f6c
Neloreck <Neloreck@gmail.com>
2026-09-11 04:05:37 +0300

    chore: Set `edition` and `rust-version` to workspace-level configuration across all packages.

--- crates/xrf-error-derive/src/lib.rs
+++ crates/xrf-error-derive/src/lib.rs
@@ -3,7 +3,7 @@ use proc_macro::TokenStream;
 use convert_case::{Case, Casing};
 use proc_macro2::{Ident, TokenStream as TokenStream2};
 use quote::quote;
-use syn::{parse_macro_input, Attribute, Data, DataEnum, DeriveInput, Error, Fields, LitStr, Meta, Variant};
+use syn::{Attribute, Data, DataEnum, DeriveInput, Error, Fields, LitStr, Meta, Variant, parse_macro_input};
 
 #[proc_macro_derive(ErrorConstructors, attributes(constructor))]
 pub fn error_constructors_derive(input: TokenStream) -> TokenStream {
